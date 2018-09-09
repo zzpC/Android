8d71a878b1bbc8a704d19452f63c9ddc19a68ad1
zzpC <a1348778498@outlook.com>
2018-09-09 20:49:39 +0800

    删除

--- Android_Base/嵌套使用LV.java
+++
@@ -1,7 +0,0 @@
-public class ARecyclerView extends {
-
-	protected void onMeasure(int widthMeasureSpec,int heightMeasure){
-		int expandSpec=MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,MeasureSpec.AT_MOST);
-		super.onMeasure(widthMeasure,expandSpec);
-	}
-}
